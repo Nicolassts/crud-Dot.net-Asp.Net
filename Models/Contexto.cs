8b69b19feaf549f3a12ded8da7c6073ec00b495b
Nicolassts <161380754+Nicolassts@users.noreply.github.com>
2024-04-10 08:03:57 -0300

    Add files via upload

---
+++ Models/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class Contexto   : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) 
+     
+            :base (options) 
+        {
+        }
+
+   
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
